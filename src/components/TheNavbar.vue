37ade8e6e0caa2b2db61a6794f235c9333a44d08
CesarF <dev.caugustoaf@gmail.com>
2024-03-12 23:41:22 -0400

    add Home page

---
+++ src/components/TheNavbar.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="navbar bg-base-100">
+  <div class="flex-1">
+    <a class="btn btn-ghost text-xl">Dev Tolls</a>
+  </div>
+  <div class="flex-none gap-2">
+    <div class="form-control">
+      <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+    </div>
+    <div class="dropdown dropdown-end">
+      <div tabindex="0" role="button" class="btn btn-ghost btn-circle avatar">
+        <div class="w-10 rounded-full">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+        </div>
+      </div>
+      <ul tabindex="0" class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+        <li>
+          <a class="justify-between">
+            Profile
+            <span class="badge">New</span>
+          </a>
+        </li>
+        <li><a>Settings</a></li>
+        <li><a>Logout</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped>
+</style>
+
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+
+</script>
+
+<template>
+  <div class="grid grid-cols-3 gap-4">
+    <div class="card w-96 bg-base-100 shadow-xl hover:bg-secondary">
+      <div class="card-body">
+        <h2 class="card-title">Convert</h2>
+        <p></p>
+      </div>
+      <figure><span class="material-icons">face</span></figure>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-xl hover:bg-secondary">
+      <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+      </div>
+      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+    </div>
+    <div class="card w-96 bg-base-100 shadow-xl hover:bg-secondary">
+      <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+          <p>If a dog chews shoes whose shoes does he choose?</p>
+      </div>
+      <figure><img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
+    </div>
+  </div>
+</template>
